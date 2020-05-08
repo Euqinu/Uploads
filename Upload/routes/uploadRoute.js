4755e0a8d725f1ff31d2d76d6f5b154778826953
unknown <uniquechhetrii@gmail.com>
2020-05-08 18:52:18 +0530

    Update:static files

--- Upload/routes/uploadRoute.js
+++ Upload/routes/uploadRoute.js
@@ -7,51 +7,21 @@ const route = express.Router();
 
 
 //Creating a client
-const gc = new Storage({
-  keyFilename: path.join(__dirname, "../keyFile.json"),
-  projectId: 'whatstrending-0013'
-});
+// const gc = new Storage({
+//   keyFilename: path.join(__dirname, "../keyFile.json"),
+//   projectId: 'whatstrending-0013'
+// });
 
-const nitrBucket = gc.bucket('nitr_2015_2019');
+const gc= new Storage();
 
-/*
-const multerConfig = {
-    
-    storage: multer.diskStorage({
-     //Setup where the user's file will go
-     destination: function(req, file, next){
-       next(null, './public/photo-storage');
-       },   
-        
-        //Then give the file a unique name
-        filename: function(req, file, next){
-            console.log(file);
-            const ext = file.mimetype.split('/')[1];
-            next(null, file.fieldname + '-' + Date.now() + '.'+ext);
-          }
-        }),   
-        
-        //A means of ensuring only images are uploaded. 
-        fileFilter: function(req, file, next){
-              if(!file){
-                next();
-              }
-            const image = file.mimetype.startsWith('image/');
-            if(image){
-              console.log('photo uploaded');
-              next(null, true);
-            }else{
-              console.log("file not supported");
-              return next();
-            }
-        }
- };
- */
+const nitrBucket = gc.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+
+
 
 const multerTest = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024,
+    fileSize: 50 * 1024 * 1024,
   },
 });
 
